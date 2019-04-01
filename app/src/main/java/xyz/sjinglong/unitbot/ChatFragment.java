afebc4acf8f83c2eed7e2fe9d418f9adb8d6505e
sjinglong <1368351931@qq.com>
2019-04-01 20:08:30 +0800

    完成聊天界面的UI

--- app/src/main/java/xyz/sjinglong/unitbot/ChatFragment.java
+++ app/src/main/java/xyz/sjinglong/unitbot/ChatFragment.java
@@ -1,7 +1,63 @@
 package xyz.sjinglong.unitbot;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatFragment extends Fragment {
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button send;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.chat_fragment, container, false);
+        initMsgs();
+        inputText = (EditText)view.findViewById(R.id.chat_fragment_edit_text);
+        send = (Button)view.findViewById(R.id.chat_fragment_send_button);
+        msgRecyclerView = (RecyclerView)view.findViewById(R.id.chat_fragment_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if (!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SEND);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                    inputText.setText("");
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void initMsgs() {
+        Msg msg1 = new Msg("游戏规则", Msg.TYPE_RECEIVE);
+        Msg msg2 = new Msg("巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉，乌拉乌拉乌拉乌拉乌拉乌拉乌拉乌拉"
+                , Msg.TYPE_RECEIVE);
+        msgList.add(msg1);
+        msgList.add(msg2);
+    }
 }
--- app/src/main/java/xyz/sjinglong/unitbot/MsgAdapter.java
+++ app/src/main/java/xyz/sjinglong/unitbot/MsgAdapter.java
@@ -2,31 +2,59 @@ package xyz.sjinglong.unitbot;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class MsgAdapter extends RecyclerView.Adapter {
-    List<Msg> msg_list;
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+    private List<Msg> mMsgList;
 
-    class ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout leftLayout;
+        RelativeLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
 
+        public ViewHolder(View view) {
+            super(view);
+            leftLayout = (RelativeLayout)view.findViewById(R.id.item_message_receive);
+            rightLayout = (RelativeLayout)view.findViewById(R.id.item_message_send);
+            leftMsg = (TextView)view.findViewById(R.id.item_robot_message);
+            rightMsg = (TextView)view.findViewById(R.id.item_user_message);
+        }
+    }
+
+    public MsgAdapter(List<Msg> msgList) {
+        mMsgList = msgList;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.chat_item, viewGroup, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Msg msg = mMsgList.get(i);
+        if (msg.getType() == Msg.TYPE_RECEIVE) {
+            viewHolder.leftLayout.setVisibility(View.VISIBLE);
+            viewHolder.rightLayout.setVisibility(View.GONE);
+            viewHolder.leftMsg.setText(msg.getContent());
+        } else if (msg.getType() == Msg.TYPE_SEND) {
+            viewHolder.rightLayout.setVisibility(View.VISIBLE);
+            viewHolder.leftLayout.setVisibility(View.GONE);
+            viewHolder.rightMsg.setText(msg.getContent());
+        }
     }
 
     @Override
     public int getItemCount() {
-        return msg_list.size();
+        return mMsgList.size();
     }
 }

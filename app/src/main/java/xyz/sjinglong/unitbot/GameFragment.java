7b319a38ec9286023c92914843d331cb1305710d
sjinglong <1368351931@qq.com>
2019-04-02 19:57:54 +0800

    添加游戏碎片，去掉actionbar

---
+++ app/src/main/java/xyz/sjinglong/unitbot/GameFragment.java
@@ -0,0 +1,20 @@
+package xyz.sjinglong.unitbot;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.game_fragment, container, false);
+        return view;
+    }
+}
